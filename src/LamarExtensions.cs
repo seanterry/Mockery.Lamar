1439fba99428cb140a9120c999c6761879609677
Sean Terry <seanterry@outlook.com>
2020-08-20 11:16:40 -0400

    ServiceRegistry extension methods (#1)

---
+++ src/LamarExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Lamar;
+
+namespace Moq
+{
+    /// <summary>
+    /// Extension methods for mocking within Lamar.
+    /// </summary>
+
+    public static class LamarExtensions
+    {
+        /// <summary>
+        /// Registers a mocked instance of the specified type with the container.
+        /// </summary>
+        /// <typeparam name="T">Type to mock.</typeparam>
+        /// <param name="registry">Lamar service registry.</param>
+        /// <param name="behavior">Mock behavior.</param>
+        /// <param name="mocked">Mocked instance.</param>
+        /// <returns>The mock setup.</returns>
+
+        public static Mock<T> Mock<T>( this ServiceRegistry registry, MockBehavior behavior, out T mocked ) where T : class
+        {
+            if ( registry == null ) throw new ArgumentNullException( nameof( registry ) );
+
+            var mock = Mockery.Of( behavior, out mocked );
+            registry.For<T>().Add( mocked );
+
+            return mock;
+        }
+
+        /// <summary>
+        /// Registers a strictly-mocked instance of the specified type with the container.
+        /// </summary>
+        /// <typeparam name="T">Type to mock.</typeparam>
+        /// <param name="registry">Lamar service registry.</param>
+        /// <param name="mocked">Mocked instance.</param>
+        /// <returns>The mock setup.</returns>
+
+        public static Mock<T> Mock<T>( this ServiceRegistry registry, out T mocked ) where T : class =>
+            registry.Mock( MockBehavior.Strict, out mocked );
+    }
+}
---
+++ test/LamarExtensionsTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Lamar;
+using Moq;
+using Xunit;
+
+namespace Mockery.Lamar.Test
+{
+    public class LamarExtensionsTests
+    {
+        public interface IMockable
+        {
+            void DoWork();
+        }
+
+        public class MockBehaviorCases : TheoryData<MockBehavior>
+        {
+            public MockBehaviorCases()
+            {
+                Add( MockBehavior.Loose );
+                Add( MockBehavior.Strict );
+            }
+        }
+
+        public class MockWithSpecificBehavior
+        {
+            ServiceRegistry registry = new ServiceRegistry();
+            MockBehavior behavior = default;
+            IMockable mocked = null;
+            Mock<IMockable> method() => registry.Mock( behavior, out mocked );
+
+            [Fact]
+            public void requires_registry()
+            {
+                registry = null!;
+                Assert.Throws<ArgumentNullException>( nameof( registry ), method );
+            }
+
+            [Theory]
+            [ClassData( typeof( MockBehaviorCases ) )]
+            public void returns_mock_with_specified_behavior( MockBehavior behavior )
+            {
+                this.behavior = behavior;
+                var mock = method();
+                Assert.Equal( behavior, mock.Behavior );
+                Assert.Same( mock.Object, mocked );
+
+                using var container = new Container( registry );
+                var actual = container.GetInstance<IMockable>();
+                Assert.Same( mock.Object, actual );
+            }
+        }
+
+        public class MockWithDefaultBehavior
+        {
+            ServiceRegistry registry = new ServiceRegistry();
+            IMockable mocked = null;
+            Mock<IMockable> method() => registry.Mock( out mocked );
+
+            [Fact]
+            public void requires_registry()
+            {
+                registry = null!;
+                Assert.Throws<ArgumentNullException>( nameof( registry ), method );
+            }
+
+            [Fact]
+            public void returns_mock_with_strict_behavior()
+            {
+                var mock = method();
+                Assert.Equal( MockBehavior.Strict, mock.Behavior );
+                Assert.Same( mock.Object, mocked );
+
+                using var container = new Container( registry );
+                var actual = container.GetInstance<IMockable>();
+                Assert.Same( mock.Object, actual );
+            }
+        }
+    }
+}
